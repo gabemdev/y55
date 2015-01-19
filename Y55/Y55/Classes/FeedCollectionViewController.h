ba226e2787c5d1a8c97dfb5956c83db866bd29fc
Gabe Morales <gabomorales@me.com>
2015-01-18 22:56:32 -0600

    Added feedCollectionview

---
+++ Y55/Y55/Classes/FeedCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedCollectionViewController.h
+//  Y55
+//
+//  Created by Rockstar. on 1/3/15.
+//  Copyright (c) 2015 Gabe Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedCollectionViewController : UICollectionViewController
+
+@end
